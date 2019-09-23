671406c8e6d3ba443837c68d6fa7fb7ca8be1428
Jeremy Mcallister <jeremy.mcallister@arteric.com>
2019-09-22 20:07:30 -0400

    wiped out the old code and started up with Gatsby

---
+++ src/components/header.js
@@ -0,0 +1,50 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+import "../styles/menu.scss"
+
+const Header = ({ siteTitle }) => (
+  <header
+    style={{
+      background: `rebeccapurple`,
+      marginBottom: `1.45rem`,
+    }}
+  >
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+      }}
+    >
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          {siteTitle}
+        </Link>
+      </h1>
+    </div>
+    <div className="menu">
+        <ul>
+            <li>hello</li>
+            <li>foo</li>
+            <li>bar</li>
+        </ul>
+    </div>
+  </header>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
